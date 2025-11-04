a1cb3b3d14e691be64e612f9ba0bea189ebe2acd
JuLieSRibeiro <ribeirojulie.js@gmail.com>
2025-11-04 16:44:01 -0300

    adicionar logo-link,  imagem no Header

--- src/Componentes/Inicio/Header/Header.jsx
+++ src/Componentes/Inicio/Header/Header.jsx
@@ -4,10 +4,18 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
+
+      <a href="/inicio" className="logo-link">
+        <img 
+          src="https://i.pinimg.com/1200x/b7/5b/29/b75b29441bbd967deda4365441497221.jpg"  // só imagem dexemplo
+          alt="Logo do Anjos Urbanos" 
+          className="header-logo"
+        />
+      </a>
+
       <input type="text" placeholder="Pesquisar sobre..." className="search-bar" />
       <nav>
         <a href="/inicio">INÍCIO</a>
-        {/* <a href="/sobre">Serviços</a> */}
         <a href="/Mensagem">MENSAGENS</a>
         <a href="/Notificações">NOTIFICAÇÕES</a>
       </nav>
@@ -16,3 +24,4 @@ function Header() {
 }
 
 export default Header;
+
