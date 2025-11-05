0b2684d52270b1ca78e7bfb0edcc75d3fad135aa
JuLieSRibeiro <ribeirojulie.js@gmail.com>
2025-11-05 19:26:29 -0300

    URL do profile-image

--- src/Componentes/Inicio/ProfileCard/ProfileCard.jsx
+++ src/Componentes/Inicio/ProfileCard/ProfileCard.jsx
@@ -3,23 +3,17 @@ import './ProfileCard.css';
 
 function ProfileCard() {
   return (
-    // <div className="profile-card">
-    //   <img src="image.png" alt="Foto do perfil" className="perfile-imagem" />
-    //   <h3 className="perfile-nome">Skilo</h3>
-    //   <p className="perfile-bio">Piriri pororo.</p>
-    // </div>
 
     <div className="profile-card">
   <div className="profile-header"></div>
   <div className="profile-image">
-    <img src="sua-imagem.jpg" alt="Foto de perfil" />
+    <img src="https://i.pinimg.com/1200x/b7/5b/29/b75b29441bbd967deda4365441497221.jpg" alt="Foto de perfil" />
   </div>
   <div className="profile-info">
     <h3>Marcela Silva</h3>
     <p>📍 Sorocaba - SP</p>
   </div>
 </div>
-
 
   );
 }
